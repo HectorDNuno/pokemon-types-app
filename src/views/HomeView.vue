67a251a4e73dbf3260dfce912857ae3f7b1ea7d7
hector <hectornuno1994@gmail.com>
2022-04-29 13:18:25 -0700

    adds functions

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,32 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
-</template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      types: [],
+    };
+  },
+  created: function () {
+    axios.get("/types.json").then((response) => {
+      this.types = response.data;
+      console.log("all types", response);
+    });
   },
 };
 </script>
+
+<template>
+  <div class="home">
+    <div v-for="type in types" :key="type.id">
+      <h4>
+        {{ type.name }}
+      </h4>
+      <p>Overview: {{ type.damage_relations_overview }}</p>
+      <p>Number of moves: {{ type.moves_with_type }}</p>
+      <p>Pokemon: {{ type.pokemon_with_type }}</p>
+      <p>Overview: {{ type.damage_relations_overview }}</p>
+      <a :href="`/types/show/${type.name}`">go to type page</a>
+    </div>
+  </div>
+</template>
