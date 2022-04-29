0f961242e99724af310c854836d44dd0ef8a4ccf
hector <hectornuno1994@gmail.com>
2022-04-29 13:19:16 -0700

    adds show page and created function to shpage

---
+++ src/views/TypesShow.vue
@@ -0,0 +1,26 @@
+<script>
+// import axios from "axios";
+
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      currentType: {},
+    };
+  },
+  created: function () {
+    axios.get("/types/" + this.$route.params.name).then((response) => {
+      console.log("show type", response);
+      this.currentType = response.data;
+    });
+  },
+};
+</script>
+
+<template>
+  <div class="types-show">
+    <h1>{{ currentType.name.charAt(0).toUpperCase() + currentType.name.slice(1) }}</h1>
+    <p>{{ currentType.damage_relations["double_damage_from"] }}</p>
+  </div>
+</template>
