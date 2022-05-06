21716aaefbf4a948b5349bc3a58f87573a9233a7
hector <hectornuno1994@gmail.com>
2022-05-06 13:14:06 -0700

    adds filter function and adds image attribute to index page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,6 +5,7 @@ export default {
   data: function () {
     return {
       types: [],
+      nameFilter: "",
     };
   },
   created: function () {
@@ -12,6 +13,15 @@ export default {
       this.types = response.data;
       console.log("all types", response.data);
     });
+  },
+  methods: {
+    // filterTypes: function () {
+    //   return this.types.filter((types) => {
+    //     var lowerName = types.name.toLowerCase();
+    //     var lowerNameFilter = this.nameFilter.toLowerCase();
+    //     return lowerName.includes(lowerNameFilter);
+    //   });
+    // },
   },
 };
 </script>
@@ -22,6 +32,7 @@ export default {
       <h4>
         {{ type.name.charAt(0).toUpperCase() + type.name.slice(1) }}
       </h4>
+      <img :src="type.image_url" :alt="type.name" />
       <p>Weaknesses: {{ type.damage_relations_overview.weaknesses }}</p>
       <p>Super-effective: {{ type.damage_relations_overview.strengths }}</p>
       <p>Neutral damage from: {{ type.damage_relations_overview.neutral }}</p>
@@ -31,3 +42,12 @@ export default {
     </div>
   </div>
 </template>
+
+<style>
+img {
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  padding: 5px;
+  width: 150px;
+}
+</style>
