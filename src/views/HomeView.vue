2f2c4befcdead298365aa55599b2844defafd323
hector <hectornuno1994@gmail.com>
2022-05-06 13:17:10 -0700

    makes image link to type page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -32,13 +32,14 @@ export default {
       <h4>
         {{ type.name.charAt(0).toUpperCase() + type.name.slice(1) }}
       </h4>
-      <img :src="type.image_url" :alt="type.name" />
+      <a :href="`/types/show/${type.name}`">
+        <img :src="type.image_url" :alt="type.name" />
+      </a>
       <p>Weaknesses: {{ type.damage_relations_overview.weaknesses }}</p>
       <p>Super-effective: {{ type.damage_relations_overview.strengths }}</p>
       <p>Neutral damage from: {{ type.damage_relations_overview.neutral }}</p>
       <p>Number of moves: {{ type.moves_with_type }}</p>
       <p>Pokemon with type: {{ type.pokemon_with_type }}</p>
-      <a :href="`/types/show/${type.name}`">go to type page</a>
     </div>
   </div>
 </template>
