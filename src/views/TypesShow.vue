014311b262d35a7bea3e4b2f9f030b2453659efb
hector <hectornuno1994@gmail.com>
2022-05-06 10:46:07 -0700

    adds loops to show names of types in damage relations, names of pokemon, and names of moves

--- src/views/TypesShow.vue
+++ src/views/TypesShow.vue
@@ -1,6 +1,4 @@
 <script>
-// import axios from "axios";
-
 import axios from "axios";
 
 export default {
@@ -11,7 +9,7 @@ export default {
   },
   created: function () {
     axios.get("/types/" + this.$route.params.name).then((response) => {
-      console.log("show type", response);
+      console.log("show type", response.data);
       this.currentType = response.data;
     });
   },
@@ -21,6 +19,53 @@ export default {
 <template>
   <div class="types-show">
     <h1>{{ currentType.name.charAt(0).toUpperCase() + currentType.name.slice(1) }}</h1>
-    <p>{{ currentType.damage_relations["double_damage_from"] }}</p>
+  </div>
+
+  <div class="damage-from">
+    <p>double_damage_from:</p>
+    <li v-for="type in currentType.damage_relations.double_damage_from" :key="type.id">
+      {{ type.name }}
+    </li>
+
+    <p>half_damage_from:</p>
+    <li v-for="type in currentType.damage_relations.half_damage_from" :key="type.id">
+      {{ type.name }}
+    </li>
+
+    <p>no_damage_from:</p>
+    <li v-for="type in currentType.damage_relations.no_damage_from" :key="type.id">
+      {{ type.name }}
+    </li>
+  </div>
+
+  <div class="damage-to">
+    <p>double_damage_to:</p>
+    <li v-for="type in currentType.damage_relations.double_damage_to" :key="type.id">
+      {{ type.name }}
+    </li>
+
+    <p>half_damage_to:</p>
+    <li v-for="type in currentType.damage_relations.half_damage_to" :key="type.id">
+      {{ type.name }}
+    </li>
+
+    <p>no_damage_to:</p>
+    <li v-for="type in currentType.damage_relations.no_damage_to" :key="type.id">
+      {{ type.name }}
+    </li>
+  </div>
+
+  <div>
+    <p>Number of Pokémon with type:</p>
+    <ol>
+      <li v-for="pokemon in currentType.pokemon" :key="pokemon.id">{{ pokemon.pokemon.name }}</li>
+    </ol>
+  </div>
+
+  <div>
+    <p>Moves with type:</p>
+    <ol>
+      <li v-for="moves in currentType.moves" :key="moves.id">{{ moves.name }}</li>
+    </ol>
   </div>
 </template>
