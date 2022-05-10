30bc0ece8193f6b72de0a35311857481e77087c0
hector <hectornuno1994@gmail.com>
2022-05-10 12:14:19 -0700

    moves data into cards, adjsusts div that was messing with sizing, moves pokemon and moves into container, adds classes to titles

--- src/views/TypesShow.vue
+++ src/views/TypesShow.vue
@@ -34,98 +34,107 @@ export default {
     <nav class="navbar navbar-light bg-light justify-content-between">
       <a class="navbar-brand" href="/">Type Checkr</a>
     </nav>
-    <div class="container"></div>
     <h1>{{ currentType.name.charAt(0).toUpperCase() + currentType.name.slice(1) }}</h1>
     <!-- <img :src="typeInfo[0].image_url" :alt="typeInfo.name" /> -->
   </div>
 
-  <div class="damage-to">
-    <div class="row">
-      <div class="col-12 col-md-4">
-        <div class="card">
+  <div class="container">
+    <div class="damage-to">
+      <span class="section-title"><h2>Damage to</h2></span>
+      <div class="card-group">
+        <div class="card mt-4">
           <div class="card-body">
-            <h5 class="card-title">Double damage to</h5>
+            <h5 class="card-title">Double</h5>
             <p v-for="type in currentType.damage_relations.double_damage_to" :key="type.id" class="card-text">
               {{ type.name }}
             </p>
           </div>
         </div>
-      </div>
 
-      <p>double_damage_to:</p>
-      <li v-for="type in currentType.damage_relations.double_damage_to" :key="type.id">
-        {{ type.name }}
-      </li>
-
-      <p>half_damage_to:</p>
-      <li v-for="type in currentType.damage_relations.half_damage_to" :key="type.id">
-        {{ type.name }}
-      </li>
-
-      <p>no_damage_to:</p>
-      <li v-for="type in currentType.damage_relations.no_damage_to" :key="type.id">
-        {{ type.name }}
-      </li>
-    </div>
-    <div class="row">
-      <div class="col-12 col-md-4">
-        <div class="card">
+        <div class="card mt-4">
           <div class="card-body">
-            <h5 class="card-title">Double damage to</h5>
-            <p v-for="type in currentType.damage_relations.double_damage_to" :key="type.id" class="card-text">
+            <h5 class="card-title">Half</h5>
+            <p v-for="type in currentType.damage_relations.half_damage_to" :key="type.id" class="card-text">
+              {{ type.name }}
+            </p>
+          </div>
+        </div>
+
+        <div class="card mt-4">
+          <div class="card-body">
+            <h5 class="card-title">No effect</h5>
+            <p v-for="type in currentType.damage_relations.no_damage_to" :key="type.id" class="card-text">
               {{ type.name }}
             </p>
           </div>
         </div>
       </div>
     </div>
-  </div>
+    <div class="damage-from">
+      <span class="section-title"><h2>Damage from</h2></span>
+      <div class="card-group">
+        <div class="card mt-4">
+          <div class="card-body">
+            <h5 class="card-title">Double</h5>
+            <p v-for="type in currentType.damage_relations.double_damage_from" :key="type.id" class="card-text">
+              {{ type.name }}
+            </p>
+          </div>
+        </div>
 
-  <div class="damage-from">
-    <p>double_damage_from:</p>
-    <li v-for="type in currentType.damage_relations.double_damage_from" :key="type.id">
-      {{ type.name }}
-    </li>
+        <div class="card mt-4">
+          <div class="card-body">
+            <h5 class="card-title">Half</h5>
+            <p v-for="type in currentType.damage_relations.half_damage_from" :key="type.id" class="card-text">
+              {{ type.name }}
+            </p>
+          </div>
+        </div>
 
-    <p>half_damage_from:</p>
-    <li v-for="type in currentType.damage_relations.half_damage_from" :key="type.id">
-      {{ type.name }}
-    </li>
+        <div class="card mt-4">
+          <div class="card-body">
+            <h5 class="card-title">No effect</h5>
+            <p v-for="type in currentType.damage_relations.no_damage_from" :key="type.id" class="card-text">
+              {{ type.name }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
 
-    <p>no_damage_from:</p>
-    <li v-for="type in currentType.damage_relations.no_damage_from" :key="type.id">
-      {{ type.name }}
-    </li>
-  </div>
+    <div class="pokemon-and-moves">
+      <h2>Pokémon with type:</h2>
 
-  <div class="pokemon-and-moves">
-    <h2>Pokémon with type:</h2>
+      <p>
+        {{
+          Array.prototype.map
+            .call(this.currentType.pokemon, function (pokemon) {
+              return pokemon.pokemon.name;
+            })
+            .join(", ")
+        }}
+      </p>
 
-    <p>
-      {{
-        Array.prototype.map
-          .call(this.currentType.pokemon, function (pokemon) {
-            return pokemon.pokemon.name;
-          })
-          .join(", ")
-      }}
-    </p>
-
-    <h2>Moves with type:</h2>
-    <p>
-      {{
-        Array.prototype.map
-          .call(this.currentType.moves, function (moves) {
-            return moves.name;
-          })
-          .join(", ")
-      }}
-    </p>
+      <h2>Moves with type:</h2>
+      <p>
+        {{
+          Array.prototype.map
+            .call(this.currentType.moves, function (moves) {
+              return moves.name;
+            })
+            .join(", ")
+        }}
+      </p>
+    </div>
   </div>
 </template>
 
 <style>
 img {
   width: 10rem;
+}
+
+.card-group {
+  display: flex;
 }
 </style>
