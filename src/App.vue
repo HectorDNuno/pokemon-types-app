9782577504e20e6f48c3bfdb62d2054f72a96d71
hector <hectornuno1994@gmail.com>
2022-05-17 11:44:19 -0700

    adds all imported font families here and changes font of navbar brand

--- src/App.vue
+++ src/App.vue
@@ -26,52 +26,11 @@ nav a {
   color: #2c3e50;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-
-.topnav {
-  overflow: hidden;
-  background-color: #e9e9e9;
-}
-
-/* Style the links inside the navigation bar */
-.topnav a {
-  float: left;
-  display: block;
-  color: black;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
-}
-
-/* Change the color of links on hover */
-.topnav a:hover {
-  background-color: #ddd;
-  color: black;
-}
-
-/* Style the "active" element to highlight the current page */
-.topnav a.active {
-  background-color: #2196f3;
-  color: white;
-}
-
-/* Style the search box inside the navigation bar */
-.topnav input[type="text"] {
-  float: right;
-  padding: 6px;
-  border: none;
-  margin-top: 8px;
-  margin-right: 16px;
-  font-size: 17px;
-}
 span.brand1 {
   color: white;
 }
 span.brand2 {
-  font-family: "Bungee Shade", cursive;
+  font-family: "Press Start 2P", cursive;
   padding-left: 5px;
   color: white;
 }
@@ -81,4 +40,5 @@ span.brand2 {
 }
 
 @import url("https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Bungee&display=swap");
 </style>
