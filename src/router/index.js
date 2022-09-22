30a1bc2fe1b6efb98c3b189f2567e8d06aacdd67
hector <hectornuno1994@gmail.com>
2022-09-22 13:18:22 -0700

    removes path for deleted file, updates import and component statements from TypesShow to TypesView, removes '-show' from name

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import TypesShow from "../views/TypesShow.vue";
+import TypesView from "../views/TypesView.vue";
 
 const routes = [
   {
@@ -9,18 +9,10 @@ const routes = [
     name: "home",
     component: HomeView,
   },
-  // {
-  // path: "/about",
-  // name: "about",
-  // route level code-splitting
-  // this generates a separate chunk (about.[hash].js) for this route
-  // which is lazy-loaded when the route is visited.
-  // component: () => import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  // },
   {
     path: "/types/show/:name",
-    name: "types-show",
-    component: TypesShow,
+    name: "types",
+    component: TypesView,
   },
 ];
 
