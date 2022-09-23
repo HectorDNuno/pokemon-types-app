6ee869d74e19c03d2e345c09fb90dd50e04a98bc
hector <hectornuno1994@gmail.com>
2022-09-22 18:30:04 -0700

    changes class names for better understanding

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,33 +1,31 @@
 <template>
-  <nav class="navbar navbar-light justify-content-between">
-    <a class="navbar-brand" href="/">
-      <div class="brand1">Type</div>
-      <div class="brand2">Checkr</div>
+  <nav class="navbar">
+    <a class="navbar-home-link" href="/">
+      <div class="navbar-title-1">Type</div>
+      <div class="navbar-title-2">Checkr</div>
     </a>
   </nav>
   <router-view />
 </template>
 
 <style>
-nav {
+.navbar {
   padding: 30px;
+  background-color: #e3313f;
 }
 
-nav a {
+.navbar-home-link {
+  display: flex;
+  font-size: 40px;
   font-weight: bold;
   color: #2c3e50;
 }
 
-.brand1 {
+.navbar-title-1 {
   color: white;
 }
-.brand2 {
+.navbar-title-2 {
   font-family: "Bungee Outline", cursive;
-  padding-left: 5px;
   color: white;
-}
-
-.navbar {
-  background-color: #e3313f;
 }
 </style>
