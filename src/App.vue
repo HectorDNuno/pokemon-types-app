7a36b1968e27ffa1cc78aa53fe49523bbdf540d0
hector <hectornuno1994@gmail.com>
2022-09-22 18:17:58 -0700

    removes navbar code and imports it as a component instead

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
+<script>
+import AppNavbar from "./components/AppNavbar.vue";
+
+export default {
+  components: {
+    AppNavbar,
+  },
+};
+</script>
+
 <template>
-  <nav class="navbar navbar-light justify-content-between">
-    <a class="navbar-brand" href="/">
-      <span class="brand1">Type</span>
-      <span class="brand2">Checkr</span>
-    </a>
-  </nav>
-  <router-view />
+  <AppNavbar />
 </template>
 
 <style>
@@ -16,28 +20,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-span.brand1 {
-  color: white;
-}
-span.brand2 {
-  font-family: "Bungee Outline", cursive;
-  padding-left: 5px;
-  color: white;
-}
-
-.navbar {
-  background-color: #e3313f;
 }
 
 @import url("https://fonts.googleapis.com/css2?family=Bungee+Outline&display=swap");
