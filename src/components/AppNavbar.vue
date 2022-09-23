0d8bbec65c9e25308e342a5e2c12572eec468a95
hector <hectornuno1994@gmail.com>
2022-09-22 18:17:15 -0700

    creates seperate component for navbar

---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav class="navbar navbar-light justify-content-between">
+    <a class="navbar-brand" href="/">
+      <div class="brand1">Type</div>
+      <div class="brand2">Checkr</div>
+    </a>
+  </nav>
+  <router-view />
+</template>
+
+<style>
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+.brand1 {
+  color: white;
+}
+.brand2 {
+  font-family: "Bungee Outline", cursive;
+  padding-left: 5px;
+  color: white;
+}
+
+.navbar {
+  background-color: #e3313f;
+}
+</style>
