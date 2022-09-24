9742575c2be0d04ca1f7d2e991575f9510da767a
hector <hectornuno1994@gmail.com>
2022-09-23 17:40:27 -0700

    removes old classes from previous code, adds card header, repositions class declarations in p tags for better readability, styles the card header to have bigger font, bungee font, white background, and border radius on top left and right corners

--- src/components/TypesCardGroup.vue
+++ src/components/TypesCardGroup.vue
@@ -13,34 +13,31 @@ export default {
 </script>
 
 <template>
-  <div class="col-lg-6 mb-4 damage-and-moves">
-    <div class="card-header-custom">
-      <h2>{{ cardGroupTitle }}</h2>
-    </div>
-
+  <div class="card-group-container">
+    <div class="card-header">{{ cardGroupTitle }}</div>
     <div class="card-group">
-      <div class="card mt-0">
+      <div class="card">
         <div class="card-body">
           <h5 class="card-title">{{ columnOneTitle }}</h5>
-          <p v-for="item in columnOneData" :key="item" class="card-text">
+          <p class="card-text" v-for="item in columnOneData" :key="item">
             {{ item.name }}
           </p>
         </div>
       </div>
 
-      <div class="card mt-0">
+      <div class="card">
         <div class="card-body">
           <h5 class="card-title">{{ columnTwoTitle }}</h5>
-          <p v-for="item in columnTwoData" :key="item" class="card-text">
+          <p class="card-text" v-for="item in columnTwoData" :key="item">
             {{ item.name }}
           </p>
         </div>
       </div>
 
-      <div class="card mt-0">
+      <div class="card">
         <div class="card-body">
           <h5 class="card-title">{{ columnThreetitle }}</h5>
-          <p v-for="item in columnThreeData" :key="item" class="card-text">
+          <p class="card-text" v-for="item in columnThreeData" :key="item">
             {{ item.name }}
           </p>
         </div>
@@ -48,3 +45,13 @@ export default {
     </div>
   </div>
 </template>
+
+<style>
+.card-header {
+  font-family: "Bungee", sans-serif;
+  font-size: 25px;
+  background-color: white !important;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+}
+</style>
